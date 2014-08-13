7767ebd2bd50756a78395cca398171f26f92b5fb
Rui Marinho <me@ruimarinho.net>
2014-08-13 22:35:26 +0100

    Initial check in, layout draft

---
+++ PixelPicker/PixelPicker/ViewModel/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace PixelPicker.ViewModel
+{
+    internal class MainViewModel : ViewModel
+    {
+        const string DefaultImageUrl = "default.png";
+        public MainViewModel()
+        {
+            CurrentColor = new SolidColorBrush(Colors.Red);
+            //   Image = new BitmapImage(new Uri("http://xamarin.com/guide/img/xs-icon.png", UriKind.RelativeOrAbsolute));
+            CurrentImage = new BitmapImage(new Uri(DefaultImageUrl, UriKind.RelativeOrAbsolute));
+        }
+
+
+        private BitmapImage _currentImage;
+
+        public BitmapImage CurrentImage
+        {
+            get { return _currentImage; }
+            set
+            {
+                _currentImage = value;
+                OnPropertyChanged("CurrentImage");
+            }
+        }
+
+        private SolidColorBrush _currentColor;
+
+        public SolidColorBrush CurrentColor
+        {
+            get { return _currentColor; }
+            set
+            {
+                _currentColor = value;
+                OnPropertyChanged("CurrentColor");
+            }
+        }
+
+
+        private ObservableCollection<string> _imageUrls = new ObservableCollection<string>();
+
+        public ObservableCollection<string> ImageUrls
+        {
+            get { return _imageUrls; }
+            set
+            {
+                _imageUrls = value;
+                OnPropertyChanged("ImageUrls");
+            }
+        }
+
+    }
+}
---
+++ PixelPicker/PixelPicker/ViewModel/ViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixelPicker.ViewModel
+{
+    internal class ViewModel : INotifyPropertyChanged
+    {
+        #region Implementation of INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, args);
+        }
+
+        #endregion
+
+    }
+}
